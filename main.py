bb7d5151dd6edba56adef131f5cfa4bd12f212a1
mehmet <mehmet.culcu2010@gmail.com>
2025-05-21 19:13:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import nst_model
+import nst
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,7 +19,7 @@ style_weight = 0.01
 lr = 0.001
 steps = 6000
 
-generated = nst_model.neural_style_transfer(content,style,content_weight,style_weight,lr,steps)
+generated = nst.neural_style_transfer(content,style,content_weight,style_weight,lr,steps)
 
 generated = generated.clone().detach().cpu()
 imshow(generated)
